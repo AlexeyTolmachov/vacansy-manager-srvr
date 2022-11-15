f22da6bb431cde37f27fcec57d05da8d9b38264c
Aleksandr-Pod <stdfire@gmail.com>
2022-11-15 22:22:46 +0200

    Not foud response

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("dist/app"));
+const app_1 = __importDefault(require("./app"));
 const port = 3000;
 // app.get("/", (req, res) => {
 //   res.send("The Main Page");
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,17 @@ import usersRouter from './routes/users';
 const app = express();
 
 app.use(cors());
-app.use('/', (req, res) => { res.send('Main Page') })
-app.use('/users', usersRouter)
+app.use('/', (req, res) => { res.send('Main Page') });
+app.use('/users', usersRouter);
+
+app.use((req, res) => {
+  console.log("no routes match");
+  res.status(404).json({ message: "Not found" });
+});
+
+// app.use(async (err, req, res, next) => {
+//   const { status = 500, message = "Server error" } = err;
+//   res.status(status).json({ message });
+// });
 
 export default app;
